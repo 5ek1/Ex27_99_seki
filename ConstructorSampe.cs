f0ef539c51660607c04c24be7a33328c839208cc
関 雄太郎 <seki@game.local>
2021-11-25 17:23:52 +0900

    プロジェクト ファイルを追加します。

---
+++ ConstructorSampe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConsoleUtility;
+
+namespace ConstructorSampe
+{
+    class ConstructorSampeMain
+    {
+        static void Main(string[] args)
+        {
+
+            Triangle test = new Triangle(1, 1);
+
+            Triangle tri = new Triangle(
+                InteractiveInput.InputFloat("辺Aの長さ："),
+                InteractiveInput.InputFloat("辺Aの長さ："),
+                InteractiveInput.InputFloat("辺Aの長さ："));
+
+            Console.WriteLine($"面積は{tri.GetSurface()}");
+            
+            
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConstructorSampe
+{
+    ///<summary>
+    ///三角形
+    ///</summary>
+
+    class Triangle
+    {
+        private float width;
+        private float height;
+        private float[] sideLength = new float[3];
+        private float surface;
+
+        ///<summary>
+        ///コンストラクタ
+        ///</summary>
+        ///<param name="w">幅</param>
+        ///<param name="h">高さ</param>
+        public Triangle(float w, float h)
+        {
+            this.width = w;
+            this.height = h;
+        }
+
+        ///<summary>
+        ///コンストラクタ
+        ///</summary>
+        ///<param name="a">辺A</param>
+        ///<param name="b">辺B</param>
+        ///<param name="c">辺C</param>
+        public Triangle(float a, float b, float c)
+        {
+            sideLength[0] = a;
+            sideLength[1] = b;
+            sideLength[2] = c;
+
+            //ヘロンの公式で面積を求める
+            float s = (sideLength[0] + sideLength[1] + sideLength[2]) / 2;
+            surface = MathF.Sqrt(s * (s - sideLength[0]) * (s - sideLength[1]) * (s - sideLength[2]));
+          
+        }
+
+        ///<summary>
+        ///面積を取得
+        ///</summary>
+        ///<returns>面積</returns>
+        public float GetSurface()
+        {
+            return surface;
+            //return width * height/2;
+        }
+
+    }
+}
